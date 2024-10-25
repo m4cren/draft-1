27670253d7457767a35c7b4668c6cb85453c3488
m4cren <156091605+m4cren@users.noreply.github.com>
2024-10-25 21:22:59 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,32 @@
+
+
+document.getElementById('login-btn').addEventListener('click',function(){
+     document.querySelector('.login-pop').style.display = 'block';
+});
+document.querySelector('.exit-login').addEventListener('click',function(){
+     var content = document.querySelector('.login-pop').style.display = 'none';
+     let loginCont = document.getElementById('login-container')
+
+     if(content == 'none'){
+          content = 'block';
+     }else{
+          loginCont.classList.add('toBottom-animation')
+          content = 'none';
+     };
+});
+document.getElementById('signup-btn').addEventListener('click',function(){
+     document.querySelector('.signup-pop').style.display = 'block';
+});
+document.querySelector('.exit-signup').addEventListener('click',function(){
+     var content = document.querySelector('.signup-pop').style.display = 'none';
+
+     if(content == 'none'){
+          content = 'block';
+     }else{
+          content = 'none';
+     };
+});
+
+document.getElementById('submit-btn').addEventListener('click', function(){
+     window.location.href = "homepage.html"
+})
